9783e5fbf65f296fc62f58cb953546bbc37dd4db
fzkfzkqq <31615678+fzkfzkqq@users.noreply.github.com>
2019-11-26 17:26:27 +1100

    first commit

---
+++ src/build/precache-manifest.e5f3f6b8b6c41fab0b2ec54a0404382b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099a3ca0d618650bad5c891df9904dd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f432e3a872ed52f9a7b",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "4ad7231b6531449daca2",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "2f432e3a872ed52f9a7b",
+    "url": "/static/js/2.57257d20.chunk.js"
+  },
+  {
+    "revision": "4ad7231b6531449daca2",
+    "url": "/static/js/main.68b356cb.chunk.js"
+  },
+  {
+    "revision": "35a7acf4fb928453b799",
+    "url": "/static/js/runtime-main.8727d782.js"
+  }
+]);
